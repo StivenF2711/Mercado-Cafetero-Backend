bdc87f6a9e40e7d8f9932be1f7d85ee712900853
StivenF2711 <141967258+StivenF2711@users.noreply.github.com>
2025-03-09 22:28:41 -0500

    Update serializers.py

--- proveedores/serializers.py
+++ proveedores/serializers.py
@@ -8,9 +8,11 @@ class CategoriaProveedorSerializer(serializers.ModelSerializer):
 
 class ProveedorSerializer(serializers.ModelSerializer):
     categoria_nombre = serializers.CharField(source="categoria.nombre", read_only=True)
+    dias_visita = serializers.SerializerMethodField()
 
     class Meta:
         model = Proveedor
-        fields = ["id", "nombre", "categoria", "categoria_nombre"]
+        fields = ["id", "nombre", "categoria", "categoria_nombre", "telefono", "email", "dias_visita"]
 
-
+    def get_dias_visita(self, obj):
+        return obj.dias_visita.split(", ")  # Convierte la cadena en lista separada por comas
