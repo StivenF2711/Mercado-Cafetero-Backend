f4c33abef9d891e84b41dcd11eae9b2ffd846f0d
StivenF2711 <141967258+StivenF2711@users.noreply.github.com>
2025-04-08 19:55:40 -0500

    act
    Co-Authored-By: julianscp <104837697+julianscp@users.noreply.github.com>

--- backend/settings.py
+++ backend/settings.py
@@ -12,7 +12,9 @@ https://docs.djangoproject.com/en/5.1/ref/settings/
 
 from pathlib import Path
 import os
+from dotenv import load_dotenv
 
+load_dotenv()  # Cargar variables desde .env
 import dj_database_url
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -58,6 +60,8 @@ INSTALLED_APPS = [
     'backend',
     'rest_framework.authtoken',
     'corsheaders',
+    'inventario',
+    'productos',
 ]
 
 MIDDLEWARE = [
@@ -97,10 +101,20 @@ WSGI_APPLICATION = 'backend.wsgi.application'
 # https://docs.djangoproject.com/en/5.1/ref/settings/#databases
 
 
-DATABASES = {
-    'default': dj_database_url.config(default=os.getenv("DATABASE_URL"))
-}
+#DATABASES = {
+#    'default': dj_database_url.config(default=os.getenv("DATABASE_URL"))
+#}
 
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': os.getenv('DB_NAME'),
+        'USER': os.getenv('DB_USER'),
+        'PASSWORD': os.getenv('DB_PASSWORD'),
+        'HOST': os.getenv('DB_HOST'),
+        'PORT': os.getenv('DB_PORT', '5432'),  # 5432 por defecto
+    }
+}
 # Password validation
 # https://docs.djangoproject.com/en/5.1/ref/settings/#auth-password-validators
 
