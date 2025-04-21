1863d279e764653a3dc6fc5210e21e7eb75d2ac9
julianscp <julians.camelop@uqvirtual.edu.co>
2025-04-21 09:17:17 -0500

    Update settings.py
    Co-Authored-By: jacobosanchezbrito <176593507+jacobosanchezbrito@users.noreply.github.com>

--- backend/settings.py
+++ backend/settings.py
@@ -163,3 +163,5 @@ STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 # https://docs.djangoproject.com/en/5.1/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+CORS_ALLOW_CREDENTIALS = True
